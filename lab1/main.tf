4bd88a353633275620b47c3f4742add8e82009c3
Marcus Lofton <mtechsoftwaresolutions1@gmail.com>
2026-04-30 17:54:14 -0500

    lab 1 - re ord main.tf and added vars.tf

--- lab1/main.tf
+++ lab1/main.tf
@@ -1,39 +1,9 @@
-data "aws_ami" "rhel_8" {
-    most_recent = true
-    owners = ["amazon"]
-
-    filter {
-        name = "name"
-        values = ["RHEL-8"]
+terraform {
+    required_providers{
+        aws = {
+        source = "hashicorp/aws"
+        version = "~> 6.0"
+        }
     }
-}
-resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/16"
-}
+} 
 
-resource "aws_instance" "instance_1" {
-    instance_type = "t2.micro"
-    ami = data.aws_ami.rhel_8
-
-    tags = {
-        Name = "instance_1_master"
-    }
-}
-
-resource "aws_instance" "instance_2" {
-    instance_type = "t2.micro"
-    ami = data.aws_ami.rhel_8
-
-    tags = {
-        Name = "instance_2_minion"
-    }
-}
-
-resource "aws_instance" "instance_3" {
-    instance_type = "t2.micro"
-    ami = data.aws_ami.rhel_8
-
-    tags = {
-        Name = "instance_3_minion"
-    }
-}
